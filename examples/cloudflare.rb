cfc2ac23cfaa58aab7e8ec5407a4b30de4187725
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2018-04-12 11:51:56 +1200

    Add cloudflare DNS over HTTP example.

---
+++ examples/cloudflare.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+gem 'async'
+gem 'async-http'
+gem 'trenni'
+
+require 'async/reactor'
+require 'async/http/client'
+require 'async/http/url_endpoint'
+
+require 'trenni/uri'
+
+# Async.logger.level = Logger::DEBUG
+
+endpoint = Async::HTTP::URLEndpoint.parse("https://cloudflare-dns.com/dns-query")
+client = Async::HTTP::Client.new(endpoint)
+
+Async::Reactor.run do |task|
+	request_uri = Trenni::URI(endpoint.url.request_uri, ct: "application/dns-json", name: "microsoft.com", type: "MX")
+	
+	puts "GET #{request_uri}"
+	response = client.get(request_uri.to_s, {})
+	
+	puts "#{response.status} #{response.version} #{response.headers.inspect}"
+	puts response.read.inspect
+end

77e0fd6f9c01ab392e36f6dc4a74ca2418e37f6d
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-04-26 17:51:03 +1200

    Increase timeout.

--- spec/async/dns/resolver_performance_spec.rb
+++ spec/async/dns/resolver_performance_spec.rb
@@ -22,86 +22,84 @@
 
 require 'async/dns'
 
-module Async::DNS::ResolverPerformanceSpec
-	describe Async::DNS::Resolver do
-		context 'benchmark' do
-			domains = %W{
-				Facebook.com
-				Twitter.com
-				Google.com
-				Youtube.com
-				Wordpress.org
-				Adobe.com
-				Blogspot.com
-				Wikipedia.org
-				Linkedin.com
-				Wordpress.com
-				Yahoo.com
-				Amazon.com
-				Flickr.com
-				Pinterest.com
-				Tumblr.com
-				W3.org
-				Apple.com
-				Myspace.com
-				Vimeo.com
-				Microsoft.com
-				Youtu.be
-				Qq.com
-				Digg.com
-				Baidu.com
-				Stumbleupon.com
-				Addthis.com
-				Statcounter.com
-				Feedburner.com
-				TradeMe.co.nz
-				Nytimes.com
-				Reddit.com
-				Weebly.com
-				Bbc.co.uk
-				Blogger.com
-				Msn.com
-				Macromedia.com
-				Goo.gl
-				Instagram.com
-				Gov.uk
-				Icio.us
-				Yandex.ru
-				Cnn.com
-				Webs.com
-				Google.de
-				T.co
-				Livejournal.com
-				Imdb.com
-				Mail.ru
-				Jimdo.com
-			}
+RSpec.describe Async::DNS::Resolver , timeout: 30 do
+	context 'benchmark' do
+		domains = %W{
+			Facebook.com
+			Twitter.com
+			Google.com
+			Youtube.com
+			Wordpress.org
+			Adobe.com
+			Blogspot.com
+			Wikipedia.org
+			Linkedin.com
+			Wordpress.com
+			Yahoo.com
+			Amazon.com
+			Flickr.com
+			Pinterest.com
+			Tumblr.com
+			W3.org
+			Apple.com
+			Myspace.com
+			Vimeo.com
+			Microsoft.com
+			Youtu.be
+			Qq.com
+			Digg.com
+			Baidu.com
+			Stumbleupon.com
+			Addthis.com
+			Statcounter.com
+			Feedburner.com
+			TradeMe.co.nz
+			Nytimes.com
+			Reddit.com
+			Weebly.com
+			Bbc.co.uk
+			Blogger.com
+			Msn.com
+			Macromedia.com
+			Goo.gl
+			Instagram.com
+			Gov.uk
+			Icio.us
+			Yandex.ru
+			Cnn.com
+			Webs.com
+			Google.de
+			T.co
+			Livejournal.com
+			Imdb.com
+			Mail.ru
+			Jimdo.com
+		}
+	
+		before do
+			require 'benchmark'
+		end
 		
-			before do
-				require 'benchmark'
-			end
-			
-			include_context Async::RSpec::Reactor
-			
-			it 'should be faster than native resolver' do
-				Benchmark.bm(30) do |x|
-					a = x.report("Async::DNS::Resolver") do
-						resolver = Async::DNS::Resolver.new([[:udp, "8.8.8.8", 53], [:tcp, "8.8.8.8", 53]])
-					
-						resolved = domains.collect{|domain| resolver.addresses_for(domain)}
-					end
-			
-					b = x.report("Resolv::DNS") do
-						resolver = Resolv::DNS.new(:nameserver => "8.8.8.8")
+		include_context Async::RSpec::Reactor
+		
+		it 'should be faster than native resolver' do
+			Benchmark.bm(30) do |x|
+				a = x.report("Async::DNS::Resolver") do
+					resolver = Async::DNS::Resolver.new([[:udp, "8.8.8.8", 53], [:tcp, "8.8.8.8", 53]])
 				
-						resolved = domains.collect do |domain|
-							[domain, resolver.getaddresses(domain)]
-						end
-					end
-					
-					# This is a regression, but not important right now.
-					# expect(a.real).to be < b.real
+					resolved = domains.collect{|domain| resolver.addresses_for(domain)}
 				end
+		
+				b = x.report("Resolv::DNS") do
+					resolver = Resolv::DNS.new(:nameserver => "8.8.8.8")
+			
+					resolved = domains.collect do |domain|
+						[domain, resolver.getaddresses(domain)]
+					end
+				end
+				
+				# This is a regression, but not important right now.
+				# expect(a.real).to be < b.real
 			end
 		end
 	end
